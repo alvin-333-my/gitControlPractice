63ae9cfddd83877ee34d3d09be99fc0cc46972a7
alvin-333-my <139318323+alvin-333-my@users.noreply.github.com>
2023-07-12 15:50:40 +0800

    Initial Commit

---
+++ gitControlPractice/gitControlPracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  gitControlPracticeApp.swift
+//  gitControlPractice
+//
+//  Created by Aaron on 2023/7/12.
+//
+
+import SwiftUI
+
+@main
+struct gitControlPracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
